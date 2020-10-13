20b1401a46fdd5d92c8be2788624e02aa284d3ed
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-10-12 21:01:39 -0300

    Criado programa que mostra qual mês é em inglês de acordo com o número

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace URI_1052
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inteiro = int.Parse(Console.ReadLine());
+
+            if (inteiro == 1){
+                Console.WriteLine("January");
+            }
+            else if (inteiro == 2){
+                Console.WriteLine("February");
+            }
+            else if (inteiro == 3){
+                Console.WriteLine("March");
+            }
+            else if (inteiro == 4){
+                Console.WriteLine("April");
+            }
+            else if (inteiro == 5){
+                Console.WriteLine("May");
+            }
+            else if (inteiro == 6){
+                Console.WriteLine("June");
+            }
+            else if (inteiro == 7){
+                Console.WriteLine("July");
+            }
+            else if (inteiro == 8){
+                Console.WriteLine("August");
+            }
+            else if (inteiro == 9){
+                Console.WriteLine("September");
+            }
+            else if (inteiro == 10){
+                Console.WriteLine("October");
+            }
+            else if (inteiro == 11){
+                Console.WriteLine("November");
+            }
+            else {
+                Console.WriteLine("December");
+            }
+        }
+    }
+}
